c2d9992dcba440aa923f4191e295bee44f535dbc
Clark Drishinski <cdrishinski@gmail.com>
2018-05-18 15:08:22 -0600

    writing function

--- sumAll.js
+++ sumAll.js
@@ -1,5 +1,13 @@
-var sumAll = function() {
-
+var sumAll = function(num1, num2) {
+let i = num1;
+if((num1 < 0)||(num2 < 0)){
+    return "Error";
 }
+
+while(num1 <= num2) {
+    let sum += num1;
+    return sum;
+}
+};
 
 module.exports = sumAll
