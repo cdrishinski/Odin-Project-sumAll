22c6f5a1805153e3bfeb457dc61486de0891e9b3
Clark Drishinski <cdrishinski@gmail.com>
2018-05-21 22:57:13 -0600

    finalized js

--- sumAll.js
+++ sumAll.js
@@ -1,13 +1,25 @@
 var sumAll = function(num1, num2) {
-let i = num1;
-if((num1 < 0)||(num2 < 0)){
-    return "Error";
-}
+let sum= 0;
 
-while(num1 <= num2) {
-    let sum += num1;
+if((num1 < 0)||(num2 < 0)){
+    return "ERROR";
+} else if ((Number.isInteger(num1)) && (Number.isInteger(num2))) {
+    if (num1 <= num2){
+        while(num1 <= num2) {
+         sum = sum + num1;
+        num1++;
+        };
+    return sum;
+    } else if (num2 <= num1){
+        while(num2 <= num1) {
+        sum = sum + num2;
+        num2++;
+        };
     return sum;
 }
-};
+} else {
+    return "ERROR";
+}
+}
 
-module.exports = sumAll
+module.exports = sumAll;
